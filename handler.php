e5a17700ec9f516bc5b36ee845944eb60c463117
MagicMarvMan <marvin.kai@gmx.de>
2021-01-18 14:43:28 +0100

    [F] Add example for form handler

---
+++ handler.php
@@ -0,0 +1,39 @@
+<?php
+// Sample handler written in PHP
+
+// Check if form fields have been sent
+if(isset($_POST["email"]) && isset($_POST["message"])) {
+    // Check if transmitted fields are PGP encrypted
+    // str_starts_with is only compatible with PHP8
+    // TODO: Ensure compatibility with PHP7
+    if(str_starts_with($_POST["email"], "-----BEGIN PGP MESSAGE-----") || str_starts_with($_POST["message"], "-----BEGIN PGP MESSAGE-----")) {
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+        // Show transmitted encrypted fields as HTML
+        // TODO: Add more complex examples such as PGP-encrypted email or adding to a database.
+        ?>
+        <b>E-Mail:</b>
+        <br>
+        <code>
+            <?php echo(htmlspecialchars($email)); ?>
+        </code>
+        <br><br>
+        <b>Message:</b>
+        <br>
+        <code>
+            <?php echo(htmlspecialchars($message)); ?> 
+        </code>
+        <?php
+    } else {
+        // TODO: Show error message in form
+        //http_response_code(302);
+        //die(header("Location: form.html"));
+        echo("Not PGP");
+    }
+} else {
+    // Set response code to 302 (temporary redirect) and redirect back to form
+    // TODO: Still needs to be improved so that error message is also displayed in the form
+    //http_response_code(302);
+    //die(header("Location: form.html"));
+    echo("Not transmitted.");
+}
