550e0cbadc0fc928f73c651e80cbba58d2bfcc74
MagicMarvMan <marvin.kai@gmx.de>
2021-01-18 11:03:18 +0100

    [I] Add check if browser supports WebCrypto RNG

--- form.js
+++ form.js
@@ -5,22 +5,26 @@ async function encrypt(message) {
     })).data;
 }
 async function submitForm() {
-    document.getElementById("submitButton").innerHTML = '<div class="spinner-border text-light" role="status"><span class="visually-hidden">Loading...</span></div>';
-    let message = document.getElementById("message").value;
-    if(message.length >= 1) {
-        let email = document.getElementById("email").value;
-        if(email.length >= 1) {
-            email = await encrypt(email);
-            message = await encrypt(message);
-            document.getElementById("message").value = message;
-            document.getElementById("email").value = email;
-            document.getElementById("contactForm").action = FORM_ACTION;
-            document.getElementById("contactForm").method = "POST";
-            document.getElementById("contactForm").submit();
+    if(window.crypto && window.crypto.getRandomValues) {
+        document.getElementById("submitButton").innerHTML = '<div class="spinner-border text-light" role="status"><span class="visually-hidden">Loading...</span></div>';
+        let message = document.getElementById("message").value;
+        if(message.length >= 1) {
+            let email = document.getElementById("email").value;
+            if(email.length >= 1) {
+                email = await encrypt(email);
+                message = await encrypt(message);
+                document.getElementById("message").value = message;
+                document.getElementById("email").value = email;
+                document.getElementById("contactForm").action = FORM_ACTION;
+                document.getElementById("contactForm").method = "POST";
+                document.getElementById("contactForm").submit();
+            } else {
+                document.getElementById("error").innerHTML = ```<p class="alert alert-danger"><b>Error:</b> Please enter your e-mail address.```;
+            }
         } else {
-            document.getElementById("error").innerHTML = ```<p class="alert alert-danger"><b>Error:</b> Please enter your e-mail address.```;
+            document.getElementById("error").innerHTML = ```<p class="alert alert-danger"><b>Error:</b> Please enter a message.```;
         }
     } else {
-        document.getElementById("error").innerHTML = ```<p class="alert alert-danger"><b>Error:</b> Please enter a message.```;
+        document.getElementById("error").innerHTML = ```<p class="alert alert-danger"><b>Error:</b> Your browser does not support a cryptographically secure random number generator. Please use a newer browser.```;
     }
 }
